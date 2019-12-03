0969196b6638d758b6fc95384e3432748cc5842d
jonvernon16 <57112508+jonvernon16@users.noreply.github.com>
2019-12-03 15:23:47 -0600

    added incomplete debugging

--- RockPaperScissors/RockPaperScissors/Program.cs
+++ RockPaperScissors/RockPaperScissors/Program.cs
@@ -6,62 +6,84 @@ namespace RockPaperScissors
     {
         static void Main(string[] args)
         {
-            
-            Random ranRockPaperScissor = new Random();
-            int randomRPS;
-            randomRPS = ranRockPaperScissor.Next(1, 3);
             int userEntry;
-
-            Console.WriteLine("Pick a number between 1-3 (1 = rock, 2 = paper 3 = scissors)");
-            userEntry = Convert.ToInt32(Console.ReadLine());
-
-            if (userEntry == 1)
+            int wins = 0;
+            int losses = 0;
+            bool again = true;
+            while (again)
             {
-                if (randomRPS == 1)
-                {
-                    Console.WriteLine("Well this is awkward I chose rock");
-                }
-                else if (randomRPS == 2)
-                {
-                    Console.WriteLine("Paper covers rock, you lose!");
-                }
-                else if (randomRPS == 3)
-                {
-                    Console.WriteLine("I lost, I chose scissors.");
-                }
-            }
-            else if (userEntry == 2)
-            {
-                if (randomRPS == 1)
-                {
-                    Console.WriteLine("I lost, I chose Rock");
-                }
-                else if (randomRPS == 2)
-                {
-                    Console.WriteLine("I guess it's a tie, I chose paper.");
-                }
-                else if (randomRPS == 3)
-                {
-                    Console.WriteLine("I won, I chose scissors!");
-                }
-            }
-            else if (userEntry == 3)
-            {
-                if (randomRPS == 1)
-                {
-                    Console.WriteLine("Rock smashes scissors, you lose!");
-                }
-                else if (randomRPS == 2)
-                {
-                    Console.WriteLine("I lost, I chose paper");
-                }
-                else if (randomRPS == 3)
-                {
-                    Console.WriteLine("It's a tie. I chose scissors");
-                }
-            }
+                
+                Random ranRockPaperScissor = new Random();
+                int randomRPS;
+                randomRPS = ranRockPaperScissor.Next(1, 3);
 
+                Console.WriteLine("Pick a number between 1-3 (1 = rock, 2 = paper 3 = scissors)");
+                userEntry = Convert.ToInt32(Console.ReadLine());
             
+                if (userEntry == 1)
+                {
+                    if (randomRPS == 1)
+                    {
+                        Console.WriteLine("Well this is awkward I chose rock");
+                    }
+                    else if (randomRPS == 2)
+                    {
+                        Console.WriteLine("Paper covers rock, you lose!");
+                        losses++;
+                    }
+                    else if (randomRPS == 3)
+                    {
+                        Console.WriteLine("I lost, I chose scissors.");
+                        wins++;
+                    }
+                }
+                else if (userEntry == 2)
+                {
+                    if (randomRPS == 1)
+                    {
+                        Console.WriteLine("I lost, I chose Rock");
+                        wins++;
+                    }
+                    else if (randomRPS == 2)
+                    {
+                        Console.WriteLine("I guess it's a tie, I chose paper.");
+                    }
+                    else if (randomRPS == 3)
+                    {
+                        Console.WriteLine("I won, I chose scissors!");
+                        losses++;
+                    }
+                }
+                else if (userEntry == 3)
+                {
+                    if (randomRPS == 1)
+                    {
+                        Console.WriteLine("Rock smashes scissors, you lose!");
+                        losses++;
+                    }
+                    else if (randomRPS == 2)
+                    {
+                        Console.WriteLine("I lost, I chose paper");
+                        wins++;
+                    }
+                    else if (randomRPS == 3)
+                    {
+                        Console.WriteLine("It's a tie. I chose scissors");
+                    }
+                }
+                if (wins == 5)
+                {
+                    Console.WriteLine("Congratulations, you really won.");
+                    again = false;
+                }
+                else if (losses == 5)
+                {
+                    Console.WriteLine("You lose, try again?");
+                    again = false;
+                }
+                Console.WriteLine("You have won " + wins + " times.");
+                Console.WriteLine("You have lossed " + losses + " times.");
+            }
         }
     }
 }
